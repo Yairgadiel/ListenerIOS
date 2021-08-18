f44db917bd7bb83cae38e361026f114215f36695
Yairgadiel <Yairgadiel@gmail.com>
2021-08-18 19:08:11 +0300

    Add existing list: * Now enabling to add a list from existing in the list addition screen. * Created matching table view and cell. * Added a method in the models returning lists a given user is not part of. * Added rollback method to the local DB, preventing unwanted behaviors. * Added a method to add a user to a records list.

--- Listener/UI/Addition/ListAdditionViewController.swift
+++ Listener/UI/Addition/ListAdditionViewController.swift
@@ -7,18 +7,36 @@
 
 import UIKit
 
-class ListAdditionViewController: UIViewController {
+class ListAdditionViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+	
+	// MARK: Outlets
 	
 	@IBOutlet weak var listId: UITextField!
 	@IBOutlet weak var listName: UITextField!
 	@IBOutlet weak var listDetails: UITextField!
 	@IBOutlet weak var loader: UIActivityIndicatorView!
+	@IBOutlet weak var existingListsTable: UITableView!
+	@IBOutlet weak var tableLoader: UIActivityIndicatorView!
+	
+	// MARK: Properteis
+	
+	var existingLists : [RecordsList]?
+	
+	// MARK: Lifecycle
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		// Do any additional setup after loading the view.
+		tableLoader.startAnimating()
+		
+		Model.instance.getNotMyLists() { data in
+			self.existingLists = data
+			self.existingListsTable.reloadData()
+			self.tableLoader.stopAnimating()
+		}
 	}
+	
+	// MARK: Outlet Actions
 	
 	@IBAction func createAction(_ sender: UIButton) {
 		let id = listId.text!
@@ -27,31 +45,65 @@ class ListAdditionViewController: UIViewController {
 		
 		// TODO validate input
 		
-		loader.startAnimating()
-		
 		// Get current user
 		let user = Model.instance.getLoggedUser()
 		
 		if (user == nil) {
 			print("No logged user")
-			self.loader.stopAnimating()
 		}
 		else {
-			Model.instance.addList(recordsList: RecordsList.create(id: id,
-																   name: name,
-																   details: details,
-																   lastUpdated: 0,
-																   userId: user!.id)) { isSuccess in
-				self.loader.stopAnimating()
-				
-				if (isSuccess) {
-					self.navigationController?.popViewController(animated: true)
-				}
-			}
+			addList(RecordsList.create(id: id,
+									   name: name,
+									   details: details,
+									   lastUpdated: 0,
+									   userId: user!.id))
 		}
 	}
 	
-	@IBAction func cancelAction(_ sender: UIButton) {
-		navigationController?.popViewController(animated: true)
+	// MARK: TableView
+	
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return self.existingLists?.count ?? 0
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = existingListsTable.dequeueReusableCell(withIdentifier: "ExistListCell", for: indexPath) as! ExistingListCell
+		
+		let currentList = (self.existingLists?[indexPath.section])!
+		
+		cell.setList(currentList)
+//		cell.selectionStyle = UITableViewCell.SelectionStyle.none
+		
+		return cell
+	}
+	
+	// method to run when table view cell is tapped
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		// note that indexPath.section is used rather than indexPath.row
+		
+		let clickedList = (self.existingLists?[indexPath.section])!
+		clickedList.add(userId: Model.instance.getLoggedUser()!.id)
+		
+		addList(clickedList)
+	}
+	
+	// There is just one row in every section
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 1
+	}
+	
+	// MARK: Methods
+	
+	// The method adds (or updates) the given list in the remote
+	func addList(_ list: RecordsList) {
+		loader.startAnimating()
+		
+		Model.instance.addList(recordsList: list) { isSuccess in
+			self.loader.stopAnimating()
+			
+			if (isSuccess) {
+				self.navigationController?.popViewController(animated: true)
+			}
+		}
 	}
 }
